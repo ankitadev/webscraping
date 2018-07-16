21b9933e89ff99b550b7e7e5f7960e43e234bd6f
ankitadev <ankitae.dev@gmail.com>
2018-07-15 23:32:50 -0400

    scrape pdf

--- src/california_scrape_PDF.py
+++ src/california_scrape_PDF.py
@@ -1,10 +1,8 @@
-import urlparse
-import urllib2
-import os
-import sys
-from bs4 import BeautifulSoup
+import urllib
 from selenium import webdriver
+import collections
 from selenium.common.exceptions import NoSuchElementException
+import csv
 
 option = webdriver.ChromeOptions()
 option.add_argument("--incognito")
@@ -12,12 +10,23 @@ option.add_argument("--disable-infobars")
 
 browser = webdriver.Chrome(executable_path='../include/chromedriver', chrome_options=option)
 
-for i in range(300000, 400000):
+myFileNumberArray = open("california_names.txt").readlines()
+
+for item in myFileNumberArray:
     try:
-        print ('\n\n\n')
-        print ('********************** page', i, '********************')
-        download_url = "https://businesssearch.sos.ca.gov/CBS/SearchResults?SearchType=NUMBER&SearchCriteria=C0" + str(i) + "&SearchSubType=Exact"
-        browser.get(download_url)
+        print ('\n')
+        business_url = "https://businesssearch.sos.ca.gov/CBS/SearchResults?SearchType=NUMBER&SearchCriteria=" + item
+        browser.get(business_url)
+
+        # define all the id's in the browser
+        titles_element = browser.find_element_by_xpath("//button[@name='EntityId']")
+        titles_element.click()
+
+        registration_key = browser.find_elements_by_xpath("//button[@class='btn btn-link docImage']")
+
+        for i in len(registration_key):
+            pdf_url = registration_key[i].get_attribute("value")
+            urllib.urlretrieve(pdf_url, "California_" + item + "_type.pdf")
 
     except NoSuchElementException:
         print("No PDF found in this page!!")
