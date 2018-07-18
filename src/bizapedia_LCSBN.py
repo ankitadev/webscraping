155369c2bbb8d9fe1bfda9c9560600aaa2f32abe
ankitadev <ankitae.dev@gmail.com>
2018-07-18 15:26:54 -0400

    bizapedia mass API request

--- src/bizapedia_LCSBN.py
+++ src/bizapedia_LCSBN.py
@@ -1,4 +1,6 @@
 import requests
+import xml.etree.ElementTree as ET
+import csv
 
 '''
 Lookup Companies - 50 max per call
@@ -10,26 +12,85 @@ have to feed:
 <c>string</c> ---- city
 
 '''
+myNameArray = open("domestic_delaware.txt").readlines()
 
 url = "https://www.bizapedia.com/bdmservice.asmx?op=LCSBN"
 name_k = "ZVJDKQEVOZOMJBDJCG"
+    #live key HKETHTSBURPBWZDTCZ
+    #test Key ZVJDKQEVOZOMJBDJCG
 name_pa = "DE"
-name_c = "Dover"
-name_n = "Genscape, Inc."
+name_c = "Wilmington"
 
-xml = """<?xml version="1.0" encoding="utf-8"?>
-<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-  <soap12:Body>
-    <LCSBN xmlns="https://www.bizapedia.com/">
-      <k>{k}</k>
-      <n>{n}</n>
-      <pa>{pa}</pa>
-      <c>{c}</c>
-    </LCSBN>
-  </soap12:Body>
-</soap12:Envelope>""".format(k=name_k,n=name_n,c=name_c,pa=name_pa)
-headers = {'Content-Type': 'application/soap+xml'}
+for ite in myNameArray:
+    print ("COMPANY NAME: ", ite)
+    name_n = ite
 
-result = requests.post(url=url,data=xml,headers=headers)
+    xml = """<?xml version="1.0" encoding="utf-8"?>
+    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+      <soap12:Body>
+        <LCSBN xmlns="https://www.bizapedia.com/">
+          <k>{k}</k>
+          <n>{n}</n>
+          <pa>{pa}</pa>
+          <c>{c}</c>
+        </LCSBN>
+      </soap12:Body>
+    </soap12:Envelope>""".format(k=name_k,n=name_n,c=name_c,pa=name_pa)
+    headers = {'Content-Type': 'application/soap+xml'}
 
-print('STATUS CODE : ',result.status_code, '\n RESPONSE : \n', str(result.content))
+    result = requests.post(url=url,data=xml,headers=headers)
+
+    #print not really necessary, just for testing purpose
+    #print('STATUS CODE : ',result.status_code, '\n RESPONSE : \n', str(result.content))
+
+    tree = ET.fromstring(str(result.content))
+
+    company = tree.findall('.//{https://www.bizapedia.com/}Company')
+    print ("COUNT: ", len(company))
+
+    with open('COUNT_Delaware_BIzapedia.csv', 'a') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        writer.writerow([ite] + [name_c] + [str(len(company))])
+    csvfile.close()
+
+    for item in tree.iter('{https://www.bizapedia.com/}Company'):
+        print ("hello loop")
+        x1=item.find('{https://www.bizapedia.com/}EntityName').text                               #Company Name
+        x2= item.find('{https://www.bizapedia.com/}FileNumber').text                               #File Number
+        x3 =item.find('.//{https://www.bizapedia.com/}FilingJurisdictionName').text                   #Filed State
+        x4= item.find('.//{https://www.bizapedia.com/}FilingJurisdictionPostalAbbreviation').text     #state
+        x5 =item.find('.//{https://www.bizapedia.com/}DomesticJurisdictionName').text
+        x6= item.find('.//{https://www.bizapedia.com/}DomesticJurisdictionPostalAbbreviation').text
+        x7 =item.find('.//{https://www.bizapedia.com/}FilingStatus').text                             #Filing status
+        x8= item.find('.//{https://www.bizapedia.com/}EntityType').text                               #Entity Type
+        x9= item.find('.//{https://www.bizapedia.com/}FilingDate').text                               #Filing Date
+        x10= item.find('.//{https://www.bizapedia.com/}PrincipalAddressCountryCode').text
+        x11= item.find('.//{https://www.bizapedia.com/}PrincipalAddressLine1').text
+        x12= item.find('.//{https://www.bizapedia.com/}PrincipalAddressLine2').text
+        x13= item.find('.//{https://www.bizapedia.com/}PrincipalAddressCity').text
+        x14= item.find('.//{https://www.bizapedia.com/}PrincipalAddressState').text
+        x15= item.find('.//{https://www.bizapedia.com/}PrincipalAddressPostalCode').text
+        x16= item.find('.//{https://www.bizapedia.com/}MailingAddressCountryCode').text
+        x17= item.find('.//{https://www.bizapedia.com/}MailingAddressLine1').text
+        x18= item.find('.//{https://www.bizapedia.com/}MailingAddressLine2').text
+        x19= item.find('.//{https://www.bizapedia.com/}MailingAddressCity').text
+        x20= item.find('.//{https://www.bizapedia.com/}MailingAddressState').text
+        x21= item.find('.//{https://www.bizapedia.com/}MailingAddressPostalCode').text
+        x22 = item.find('.//{https://www.bizapedia.com/}RegisteredAgentName').text                      #Registered Agent Name
+        x23 = item.find('.//{https://www.bizapedia.com/}RegisteredAgentAddressCountryCode').text        #Registered Agent Address C ountry Code
+        x24 = item.find('.//{https://www.bizapedia.com/}RegisteredAgentAddressLine1').text              #Registered Agent Address Line1
+        x25 = item.find('.//{https://www.bizapedia.com/}RegisteredAgentAddressLine2').text
+        x26 = item.find('.//{https://www.bizapedia.com/}RegisteredAgentCity').text                      #Registered Agent City
+        x27 = item.find('.//{https://www.bizapedia.com/}RegisteredAgentState').text                     #Registered Agent State
+        x28 = item.find('.//{https://www.bizapedia.com/}RegisteredAgentPostalCode').text                #Registered Agent Postal Code
+        x29 = item.find('.//{https://www.bizapedia.com/}RegisteredAgentPhone').text
+        x30 = item.find('.//{https://www.bizapedia.com/}RegisteredAgentFax').text
+        x31 = item.find('.//{https://www.bizapedia.com/}RegisteredAgentEmail').text
+        x32 = item.find('.//{https://www.bizapedia.com/}LastUpdateDate').text                           #Last Update Date
+        x33 = item.find('.//{https://www.bizapedia.com/}RelevanceScore').text                           #Relevance Score
+
+        with open('Delaware_California_BIzapedia.csv', 'a') as csvfile:
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            writer.writerow([x1]+[x2]+[x3]+[x4]+[x5]+[x6]+[x7]+[x8]+[x9]+[x10]+[x11]+[x12]+[x13]+[x14]+[x15]+[x16]+[x17]+[x18]+[x19]+[x20]+[x21]+[x22]+[x23]+[x24]+[x25]+[x26]+[x27]+[x28]+[x29]+[x30]+[x31]+[x32]+[x33])
+        csvfile.close()
+
